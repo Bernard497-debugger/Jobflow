6bcc263df840e802fccf46a3414d4516c452943c
Bernard497-debugger <botsile55@gmail.com>
2026-04-01 02:59:38 +0200

    Modify app run configuration for port setting
    Update app to run on specified port from environment variable

--- app.py
+++ app.py
@@ -187,6 +187,7 @@ def search():
     <a href='/dashboard'>Back</a>
     """
 
-# ---------- RUN ----------
+# ---------- RUN --------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)

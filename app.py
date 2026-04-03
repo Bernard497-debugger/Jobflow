4f4823c95dd6bc2ce85ca28d3559198357ef5316
Bernard497-debugger <botsile55@gmail.com>
2026-04-03 05:59:19 +0200

    Modify sitemap.xml to add login and signup URLs
    Updated sitemap to include login and signup URLs.

--- app.py
+++ app.py
@@ -283,10 +283,10 @@ def google_verification():
 def sitemap():
     xml = '''<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url><loc>https://jobflow.onrender.com/</loc><priority>1.0</priority></url>
+  <url><loc>https://jobflow.onrender.com/login</loc><priority>1.0</priority></url>
+  <url><loc>https://jobflow.onrender.com/signup</loc><priority>0.9</priority></url>
   <url><loc>https://jobflow.onrender.com/about</loc><priority>0.8</priority></url>
   <url><loc>https://jobflow.onrender.com/privacy</loc><priority>0.7</priority></url>
-  <url><loc>https://jobflow.onrender.com/pricing</loc><priority>0.9</priority></url>
 </urlset>'''
     return app.response_class(xml, mimetype='application/xml')
 
